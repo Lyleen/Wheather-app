a4789f54db46950778c9b8422cbd837ca9d8ead3
Kate <kathleen.ccd@gmail.com>
2022-11-27 13:08:58 +0100

    API SheChodes

--- wheather_java.js
+++ wheather_java.js
@@ -14,7 +14,7 @@ let currentHour = now.getHours();
 let currentMinutes = now.getMinutes();
 
 let day = document.querySelector("#current_day");
-if (currentMinutes >= 9) {
+if (currentMinutes > 9) {
   day.innerHTML = `${currentDay} ${currentHour}:${currentMinutes}`;
 } else {
   day.innerHTML = `${currentDay} ${currentHour}:0${currentMinutes}`;
@@ -22,28 +22,59 @@ if (currentMinutes >= 9) {
 
 //load input the city & it's temp
 function newdata(response) {
-  let newcityname = response.data.name;
+  console.log(response);
+  let newcityname = response.data.city;
   let showthecity = document.querySelector("#current_place");
   showthecity.innerHTML = newcityname;
-  let citytemp = Math.round(response.data.main.temp);
+  let citytemp = Math.round(response.data.temperature.current);
   let showtemp = document.querySelector("#temperature");
   showtemp.innerHTML = citytemp + "°";
   let citywind = response.data.wind.speed;
   let showind = document.querySelector("#wind");
   showind.innerHTML = `Wind : ${citywind} km/h`;
-  let cityhum = response.data.main.humidity;
+  let cityhum = response.data.temperature.humidity;
   let showhumd = document.querySelector("#humidity");
-  showhumd.innerHTML = `Humidity : ${cityhum} `;
+  showhumd.innerHTML = `Humidity : ${cityhum} %`;
+  let descr = response.data.condition.description;
+  let newdescr = document.querySelector("#description");
+  newdescr.innerHTML = descr;
+  let emoji = document.querySelector("emoji");
+  emoji.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon_url}.png`
+  );
+  emoji.setAttribute("alt", response.data.condition.description);
 }
+let urlcity = `https://api.shecodes.io/weather/v1/current?query=Honolulu&key=tfc5b1174a6eb0eo33d062c2b145a43f&units=metric`;
+axios.get(urlcity).then(newdata);
+
 function loadingdata(event) {
   event.preventDefault();
-  let apiKey = "5863935ee9cca4c02ed68203f807c65b";
-  let urlcity = `https://api.openweathermap.org/data/2.5/weather?q=${inputcity.value}&appid=${apiKey}&units=metric`;
+  let urlcity = `https://api.shecodes.io/weather/v1/current?query=${inputcity.value}&key=tfc5b1174a6eb0eo33d062c2b145a43f&units=metric`;
   axios.get(urlcity).then(newdata);
 }
 let inputcity = document.querySelector("#cityname");
 let thenewcity = document.querySelector("#countryform");
 thenewcity.addEventListener("submit", loadingdata);
+
+//convertion temperature units
+function farhenheit(event) {
+  event.preventDefault();
+  let targettemperature = document.querySelector("#temperature");
+  let farhenheit = (targettemperature * 9) / 5 + 32;
+  temperature.innerHTML = Math.ceil(farhenheit);
+}
+
+function celciuss(event) {
+  event.preventDefault();
+  let targettemperature = document.querySelector("#temperature");
+  temperature.innerHTML = targettemperature;
+}
+
+let clickonF = document.querySelector("#farh");
+clickonF.addEventListener("click", farhenheit);
+let clickonC = document.querySelector("#celcius");
+clickonC.addEventListener("click", celciuss);
 
 //load currentposition data
 let showcurrentcityname = document.querySelector("#positionbutton");
@@ -53,23 +84,22 @@ function launch() {
   navigator.geolocation.getCurrentPosition(TheCurrentPosition);
 }
 function TheCurrentPosition(position) {
-  let apiKey = "5863935ee9cca4c02ed68203f807c65b";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let urlloc = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  let urlloc = `https://api.shecodes.io/weather/v1/current?lat=${lat}&lon=${lon}&key=tfc5b1174a6eb0eo33d062c2b145a43f&units=metric`;
   axios.get(urlloc).then(Loadnewdataposition);
 }
 function Loadnewdataposition(response) {
-  let currentcityname = response.data.name;
+  let currentcityname = response.data.city;
   let showthecurrentcity = document.querySelector("#current_place");
   showthecurrentcity.innerHTML = "📍 " + currentcityname;
-  let temperature = Math.round(response.data.main.temp);
+  let temperature = Math.round(response.data.temperature.current);
   let currenttemp = document.querySelector("#temperature");
   currenttemp.innerHTML = temperature + "°";
   let citywnd = response.data.wind.speed;
   let displaywind = document.querySelector("#wind");
   displaywind.innerHTML = `Wind : ${citywnd} km/h`;
-  let cityhumid = response.data.main.humidity;
+  let cityhumid = response.data.temperature.humidity;
   let displayhumd = document.querySelector("#humidity");
-  showhumd.innerHTML = `Humidity : ${cityhumid} `;
+  showhumd.innerHTML = `Humidity : ${cityhumid} %`;
 }
